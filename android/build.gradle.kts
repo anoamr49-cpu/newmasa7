c5883d3d50471f8a6f6447b107e4efb5f221d5e5
anoamr49-cpu <anoamr49@gmail.com>
2026-09-16 19:51:30 +0300

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,25 +1,4 @@
 plugins {
-    // Flutter/Kotlin plugins are loaded by settings.gradle.kts.
-    // Do not declare them again here.
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-rootProject.buildDir = "../build"
-
-subprojects {
-    project.buildDir = "${rootProject.buildDir}/${project.name}"
-}
-
-subprojects {
-    project.evaluationDependsOn(":app")
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.buildDir)
+    id("com.android.application") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
 }
