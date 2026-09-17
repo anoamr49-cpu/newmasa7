2e33d86a7beb8cd1d24425b8f543cc87fce5f364
Amr Gamal Awad <122135155+amrGamal210@users.noreply.github.com>
2026-09-17 19:45:57 +0300

    N

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,14 +5,6 @@ allprojects {
     }
 }
 
-val newBuildDir = rootProject.layout.buildDirectory.dir("../build").get()
-rootProject.layout.buildDirectory.set(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir = newBuildDir.dir(project.name)
-    layout.buildDirectory.set(newSubprojectBuildDir)
-}
-
 subprojects {
     project.evaluationDependsOn(":app")
 }
